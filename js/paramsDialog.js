040e87673f4774dff648a155f07b737ac301ee0c
Beliy Zurab <beliy@reksoft.ru>
2016-12-27 11:24:33 +0300

    Cохранение в локальной базе данных

---
+++ js/paramsDialog.js
@@ -0,0 +1,52 @@
+/**
+ * Created by beliy on 27.12.2016.
+ */
+
+/**
+ * Create and show dialog for editing params
+ * @param title title of dialog
+ * @param json current params json
+ * @param onSave on save button click callback
+ */
+function showParamsDialog(title, json, onSave) {
+    const html = `
+        <div class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog  modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h4 class="modal-title">${title}</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="alert alert-warning" role="alert">
+                            <strong>Внимание!</strong> После сохранения все введенные данные на странице будут удалены.
+                        </div>
+                        <div class="alert alert-info" role="alert">
+                            Проверок правильности JSON'a пока никаких нет. Пишите сразу правильно. 
+                        </div>
+                        <textarea class="form-control js-params">${JSON.stringify(json, null, 4)}</textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="btn btn-default" data-dismiss="modal">Отмена</div>
+                        <div class="btn btn-primary js-save-btn">Сохранить</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    const $modal = $(html);
+    const $paramsArea = $modal.find('textarea.js-params');
+    $modal.on('click', '.js-save-btn', function () {
+        onSave(JSON.parse($paramsArea.val()));
+        $modal.modal('hide');
+    });
+    $modal.on('hidden.bs.modal', function () {
+        $modal.remove();
+    });
+    $modal.on('shown.bs.modal', function () {
+        resizeTextArea($paramsArea[0]);
+    });
+    $modal.modal('show');
+}
---
+++ js/script.js
@@ -0,0 +1,167 @@
+/**
+ * Created by beliy on 21.12.2016.
+ */
+
+(function () {
+    const
+        $body = $('body'),
+        $content = $('#content'),
+        $resultContent = $('#resultContent'),
+        $resultTable = $('#resultTable');
+
+    const db = new PouchDB('testCases');
+
+    let currentParams = {
+            "headerParams": {
+                "used": false,
+                "rows": [{"code": "", "colspan": 0, "valueColspan": 0, "width": "", "name": "", "inInputs": false, "inResult": false}]
+            },
+            "blocks": [{
+                "code": "",
+                "title": {"text": "", "colspan": 0},
+                "cells": [{"code": "", "colspan": 0, "width": "", "name": "", "inInputs": false, "inResult": false, "isOrderNumber": false}]
+            }]
+        },
+
+        currentTestCase = {
+            headerValues: {"[rowCode]": "[value]"},
+            blocksValues: {"[blockCode]": [{"[cellCode]": "[value]"}]}
+        };
+
+    const
+        appendItem = function (blockParams, values) {
+            const $itemsTable = $(`#${blockParams.code}`);
+
+            let rowContent = '';
+            for (let cellParam of blockParams.cells) {
+                if (cellParam.inInputs) {
+                    rowContent += `
+                        <td width="${cellParam.width || ''}">
+                            <textarea data-cell-code="${cellParam.code || ''}" class="form-control" 
+                                      placeholder="${cellParam.name || ''}">${values && values[cellParam.code] || ''}</textarea>
+                        </td>
+                    `;
+                }
+            }
+            $itemsTable.append(`
+                <tr class="js-item draggable">
+                    <td width="1%"><i class="fa fa-arrows-v big-icon margin-top-10"></i></td>
+                    ${rowContent}
+                    <td width="1%"><i class="fa fa-remove clickable big-icon margin-top-10 js-remove-item"></i></td>
+                </tr>
+            `);
+        },
+
+        rebuildPageContent = function () {
+            $content.html(generatePageContent(currentParams, currentTestCase));
+            for (let blockParams of currentParams.blocks) {
+                if (currentTestCase && currentTestCase.blocksValues && currentTestCase.blocksValues[blockParams.code]) {
+                    for (let rowValues of currentTestCase.blocksValues[blockParams.code]) {
+                        appendItem(blockParams, rowValues);
+                    }
+                }
+            }
+            $('.js-input-data-table').sortable({
+                items: ">.draggable"
+            });
+        },
+
+        saveParams = function (newParams) {
+            newParams._id = 'params';
+            if (currentParams._rev) {
+                newParams._rev = currentParams._rev;
+            }
+            db.post(newParams)
+                .then(function (response) {
+                    currentParams = newParams;
+                    currentParams._rev = response.rev;
+                    rebuildPageContent(currentParams);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        },
+
+        initParams = function () {
+            db.get('params')
+                .then(function (params) {
+                    currentParams = params;
+                    rebuildPageContent();
+                })
+                .catch(function (err) {
+                    if (err.status === 404) {
+                        $.getJSON('contentParams.json', function (json) {
+                            saveParams(json);
+                        });
+                    }
+                    console.log(err);
+                });
+        };
+
+
+    $(document).ready(function () {
+        initParams();
+
+        $body.on('click', '.js-do-magic', function () {
+            const result = generateResult(currentParams);
+            currentTestCase = result.testCase;
+            $resultTable.html(result.html);
+            $resultContent.show();
+        });
+
+        $body.on('click', '.js-add-item', function (e) {
+            const $target = $(e.currentTarget);
+            const blockCode = $target.data('blockCode');
+            for (let blockParams of currentParams.blocks) {
+                if (blockParams.code === blockCode) {
+                    appendItem(blockParams);
+                }
+            }
+        });
+
+        $body.on('click', '.js-remove-item', function (e) {
+            $(e.currentTarget).closest('tr').remove();
+        });
+
+        $body.on('keyup', 'textarea', function (e) {
+            resizeTextArea(e.currentTarget);
+        });
+
+        $body.on('keyup paste change', 'textarea,input', function () {
+            $resultTable.html('');
+            $resultContent.hide();
+        });
+
+        $body.on('click', '.js-settings-button', function () {
+            let json = $.extend(true, {}, currentParams);
+            delete json._id;
+            delete json._rev;
+            showParamsDialog('Редактирование параметров', json, saveParams);
+        });
+
+        $body.on('click', '.js-download-xls', function () {
+            convertToFile($resultTable, 'testCases', 'xls');
+        });
+
+        $body.on('click', '.js-download-doc', function () {
+            convertToFile($resultTable, 'testCases', 'doc');
+        });
+
+        $body.on('click', '.js-save-in-db', function () {
+            db.post(currentTestCase).then(function (response) {
+                console.log(JSON.stringify(response));
+            });
+        });
+    });
+})();
+
+/**
+ * Calculate textarea height by included content
+ * @param element textarea DOM-element
+ */
+function resizeTextArea(element) {
+    element.style.height = '5px';
+    element.style.height = (element.scrollHeight) + 'px';
+    element.blur();
+    element.focus()
+}
