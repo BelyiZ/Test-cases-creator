cd92cc140dded3697b4a1fe0e8cbaf5cd9c4e1e4
Beliy Zurab <beliy@reksoft.ru>
2017-03-24 15:01:18 +0300

    Функция сравнения массивов

--- js/ru/belyiz/utils/ArraysUtils.js
+++ js/ru/belyiz/utils/ArraysUtils.js
@@ -20,4 +20,20 @@
         return index >= 0 && array && index < array.length ? array[index] : defaultValue;
     };
 
+    /**
+     * Сверяет два массива на идентичность элементов.
+     * @param array1 первый массив
+     * @param array2 второй массив
+     * @param orderDependent (опционально) учитывать порядок элементов или нет
+     * @returns {boolean} возвращает false в случае, если хотя бы один из параметров (array1, array2) не массив
+     */
+    ArraysUtils.prototype.compare = function (array1, array2, orderDependent) {
+        if (!Array.isArray(array1) || !Array.isArray(array2) || array1.length !== array2.length) {
+            return false;
+        }
+        return orderDependent ?
+            array1.every((element, index) => element === array2[index]) :
+            $(array1).not(array2).length === 0 && $(array2).not(array1).length === 0;
+    };
+
 })(window, window.ru.belyiz.utils);
